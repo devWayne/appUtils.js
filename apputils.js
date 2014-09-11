365336aa82010c50033c282fafd04659b4701ece
Wayne <dev.wayne.ix@gmail.com>
2014-09-11 09:38:48 +0800

    init

---
+++ apputils.js
@@ -0,0 +1,36 @@
+  function openScheme(dianpingUrl, downloadUrl, callback) {
+      var userAgent = window.navigator.userAgent;
+      var redirect_url = "";
+      if (userAgent.match("Android") || userAgent.match("iPhone") || userAgent.match("iPad")) {
+          startTime = Date.now();
+          div = document.createElement('div');
+          div.style.visibility = 'hidden';
+          // alert('start ifrmae');
+          div.innerHTML = '<iframe id="schema" src="' + dianpingUrl + '" scrolling="no" width="1" height="1"></iframe>';
+          document.body.appendChild(div);
+
+          var delta = Date.now() - startTime;
+          // alert(delta);
+          if (delta < 50) {
+              redirect_url = downloadUrl;
+          }
+          // alert(delta);
+      } else {
+          redirect_url = downloadUrl;
+      }
+      if (redirect_url) {
+          // _hip.push(['mv', {module:'dpapp_pay_share_download',action:'click'}]);
+          if (redirect_url !== 'callback') {
+              setTimeout(function() {
+                  window.location = redirect_url;
+              }, 500);
+          } else if (callback) {
+              callback();
+          }
+      }
+  }
+
+   //openDPScheme(
+   //           "dianping2://recommendshop?url=http%3A%2F%2Fhd.t.dianping.com%2Fqiang%2Fm%2Fdeal%2F6075607%3Fversion%3D*%26agent%3D*%26cityid%3D*%26token%3D*", 
+   //            "http://m.api.dianping.com/downloadlink?redirect=3222"
+   //        );
