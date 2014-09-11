18c9a80614bb43bc927a3a13ba9e8f1fa9b75d6f
Wayne <dev.wayne.ix@gmail.com>
2014-09-11 17:28:54 +0800

    update

--- apputils.js
+++ apputils.js
@@ -34,3 +34,19 @@
    //           "dianping2://recommendshop?url=http%3A%2F%2Fhd.t.dianping.com%2Fqiang%2Fm%2Fdeal%2F6075607%3Fversion%3D*%26agent%3D*%26cityid%3D*%26token%3D*", 
    //            "http://m.api.dianping.com/downloadlink?redirect=3222"
    //        );
+  
+  function checkAgent(redirectUrl){
+         var ua = navigator.userAgent;
+            var isAndroid = ua.match(/(android)\s+([\d.]+)/i);
+
+            var isIos = ua.match(/(ipad|iphone|ipod).*os\s([\d_]+)/i);
+
+            var isIPad = ua.match(/(ipad).*os\s([\d_]+)/i);
+
+            if (!isAndroid && !isIos && !isIPad) {
+                location.href = redirectUrl;
+                return;
+            }
+  }
+
+   
